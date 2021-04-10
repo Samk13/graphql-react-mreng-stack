fe5fb92c6f073e8117fa820385c0a39bfcf4792f
Samk13 <samarbid13@gmail.com>
2021-04-10 16:55:36 +0200

    :hammer: refactor gql and add nodemon

--- index.js
+++ index.js
@@ -1,48 +1,23 @@
-const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
+const { ApolloServer } = require('apollo-server')
 const mongoose = require('mongoose');
 const { MONGODB } = require('./config.js')
-
-const Post = require('./models/Post')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 
 const PORT = 3001
-
-const typeDefs = gql`
-    type Post {
-        id: ID!
-        body: String!
-        createdAt: String!
-        username: String!
-    }
-
-    type Query{
-        getPosts: [Post]
-    }
-`
-
-const resolvers = {
-    Query: {
-        getPosts: async () => {
-            try {
-                const posts = await Post.find()
-                return posts
-            } catch (error) {
-                throw new Error(error)
-            }
-        }
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
-mongoose.connect(MONGODB, {useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose
+    .connect(MONGODB, {useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => {
         console.log('connect to MONGODB successfully 🤙🏼');
+
         return server.listen({
             port: PORT
         })
